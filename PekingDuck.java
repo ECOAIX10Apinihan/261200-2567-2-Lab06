702fda0e13d63ebc2a91897db22bd177026c8a51
JJ <kongpopsattarach.edu@gmail.com>
2024-12-25 16:26:47 +0700

    Rename BeijingDuck class to PekingDuck and update method names accordingly

--- PekingDuck.java
+++ PekingDuck.java
@@ -1,12 +1,12 @@
-public class BeijingDuck extends Duck{
-    public BeijingDuck(String name){
+public class   PekingDuck extends Duck{
+    public PekingDuck(String name){
         super(name);
     }
     @Override
     public void clean(Animal animal){
         System.out.println(this + " splash splash " + animal);
     }
-    public void beijingDuck(){
+    public void pekingDuck(){
         System.out.println(" I am a Beijing Duck ");
     }
     
