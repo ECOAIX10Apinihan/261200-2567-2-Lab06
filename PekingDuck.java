ed8b4bc5aa12469e862d9da58a4d33645d0019d8
JJ <kongpopsattarach.edu@gmail.com>
2024-12-25 16:27:08 +0700

    Update PekingDuck class to reflect name change in output message

--- PekingDuck.java
+++ PekingDuck.java
@@ -7,7 +7,7 @@ public class   PekingDuck extends Duck{
         System.out.println(this + " splash splash " + animal);
     }
     public void pekingDuck(){
-        System.out.println(" I am a Beijing Duck ");
+        System.out.println(" I am a PekingDuck Duck ");
     }
     
 }
